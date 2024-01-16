583aadfc1de06e41137b3d2fab0e3586a3e8d085
gavinBP <gavin.briggs_perez@protonmail.com>
2024-01-16 11:13:28 -0500

    added server.js and reconfigured start scripts in json file accordingly

---
+++ src/server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/*', (req, res) => {
+    res.redirect('/');
+})
+
+//global error handler
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+
+
+  app.listen(3000, () => console.log('Backend is in Business!!!'));
